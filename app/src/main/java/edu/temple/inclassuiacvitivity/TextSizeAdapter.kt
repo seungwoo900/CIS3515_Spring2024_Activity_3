7065636cd7a8be398701ae38bd5b7c35c2c6bf61
Daniel Lee <tur95049@temple.edu>
2024-02-08 12:19:44 -0500

    implemented custom adapter

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,46 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Spinner
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(_context: Context, _array: Array<Int>) : BaseAdapter() {
+
+    private val context = _context
+    private val array = _array
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return array.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return array[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val textView = TextView(context)
+        textView.text = array[position].toString()
+        val numberSize = array[position]
+        textView.textSize = 22f
+        textView.setPadding(5,10,0,10)
+        return textView
+
     }
 
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val textView = TextView(context)
+        textView.text = array[position].toString()
+        val numberSize = array[position]
+        textView.textSize = numberSize.toFloat()
+        textView.setPadding(5,10,0,10)
+
+        return textView
+    }
 }
